bb566c155d0bcb87e57776d144f6c8b1c308a1f7
bruno-lombardi <djfrizer1@gmail.com>
2021-04-01 20:18:24 -0300

    prettier and husky

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export function sum (...values: number[]): number {
-  return values.reduce((x, y) => x + y)
+export function sum(...values: number[]):number {
+      return values.reduce((x,y) => x+y)
 }
