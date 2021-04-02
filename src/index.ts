00436308a864f890147d961e7434c00444067abf
bruno-lombardi <djfrizer1@gmail.com>
2021-04-01 21:03:04 -0300

    change husky

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,8 @@ export function sub(...values: number[]):number {
        return values
        .reduce((x,y) => x-y)
 }
+
+export function mult(...values: number[]):number {
+       return values
+       .reduce((x,y) => x*y)
+}
