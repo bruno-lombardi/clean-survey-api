058f215c664155797e286c8486069dcf22c12eac
bruno-lombardi <djfrizer1@gmail.com>
2021-04-02 09:27:01 -0300

    chore: add jest and jest config
    chore: add jest and create first tests

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
--- src/math.ts
+++ src/math.ts
@@ -10,6 +10,10 @@ export function mult(...values: number[]): number {
   return values.reduce((x, y) => x * y)
 }
 
+export function div(x: number, y: number): number {
+  return x / y
+}
+
 export function sqr(value: number): number {
   return Math.sqrt(value)
 }
