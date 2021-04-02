14c13cd997014e2857a65ee951909340798bd0ba
bruno-lombardi <djfrizer1@gmail.com>
2021-04-01 21:01:01 -0300

    test linter

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export function sum(...values: number[]):number {
        return values
        .reduce((x,y) => x+y)
 }
+
+export function sub(...values: number[]):number {
+       return values
+       .reduce((x,y) => x-y)
+}
