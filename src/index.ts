d9919efa2804b94b2a8ca5f0a3bc5a019bdbc29b
bruno-lombardi <djfrizer1@gmail.com>
2021-04-02 08:59:31 -0300

    testing eslint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-export function sum(...values: number[]):number {
-       return values
-       .reduce((x,y) => x+y)
+export function sum(...values: number[]): number {
+  return values.reduce((x, y) => x + y)
 }
 
-export function sub(...values: number[]):number {
-       return values
-       .reduce((x,y) => x-y)
+export function sub(...values: number[]): number {
+  return values.reduce((x, y) => x - y)
 }
 
-export function mult(...values: number[]):number {
-       return values
-       .reduce((x,y) => x*y)
+export function mult(...values: number[]): number {
+  return values.reduce((x, y) => x * y)
 }
