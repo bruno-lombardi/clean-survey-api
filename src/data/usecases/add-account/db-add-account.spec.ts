7e1dc50c26e864e28c71f560ca2f2c3770a59831
bruno-lombardi <djfrizer1@gmail.com>
2021-04-11 20:47:45 -0300

    feat: db add account use case implementation

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,55 @@
+import { Encrypter } from '../../protocols/encrypter'
+import { DbAddAccount } from './db-add-account'
+
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub {
+    async encrypt(value: string): Promise<string> {
+      return await new Promise((resolve) => resolve('hashed_value'))
+    }
+  }
+  return new EncrypterStub()
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
+
+describe('DbAddAccount UseCase', () => {
+  it('should call encrypter with correct password', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+    await sut.add(accountData)
+    expect(encryptSpy).toHaveBeenLastCalledWith('valid_password')
+  })
+
+  it('should throw if encrypter throws exception', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
+})
---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from '../../../domain/models/account'
+import {
+  AddAccount,
+  AddAccountModel
+} from '../../../domain/usecases/add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+export class DbAddAccount implements AddAccount {
+  constructor(private readonly encrypter: Encrypter) {}
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return await new Promise((resolve) => resolve({} as AccountModel))
+  }
+}
