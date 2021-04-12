e22a072fc6fb23416efac9c4a05642f808158db4
bruno-lombardi <djfrizer1@gmail.com>
2021-04-11 21:47:37 -0300

    feat: create infra layer and add bcrypt adapter implementation

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt'
+import { Encrypter } from '../../data/protocols/encrypter'
+
+export class BCryptAdapter implements Encrypter {
+  constructor(private readonly saltRounds: number) {}
+
+  async encrypt(value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.saltRounds)
+    return hash
+  }
+}
