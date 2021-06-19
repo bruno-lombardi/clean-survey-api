94d4f9b08977e7aa94285b2f64775b21bba9ef38
bruno-lombardi <djfrizer1@gmail.com>
2021-06-18 23:06:55 -0300

    chore: add build scripts and tsc config

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,18 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+// import fg from 'fast-glob'
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async (file) =>
-    (await import(`../../../${file}`)).default(router)
-  )
+  const routesFolder = resolve(__dirname, '..', 'routes')
+  readdirSync(routesFolder).map(async (file) => {
+    if (!file.includes('.test.')) {
+      ;(await import(resolve(routesFolder, file))).default(router)
+    }
+  })
+  // fg.sync('**/src/main/routes/**routes.ts').map(async (file) =>
+  //   (await import(`../../../${file}`)).default(router)
+  // )
 }
